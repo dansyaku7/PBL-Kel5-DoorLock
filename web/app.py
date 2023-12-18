24fda6dfb8a98df92934cb395cf9cfcae701a792
dansyaku7 <syakuronabdan49@gmail.com>
2023-12-19 05:59:24 +0700

    NewFile

--- web/app.py
+++ web/app.py
@@ -1,13 +1,14 @@
-import os
+from datetime import datetime
 from flask import Flask, render_template, request, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/db_websmartdoorlock'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/doorlock_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'  # Ganti dengan kunci rahasia yang aman
+app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -16,6 +17,14 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+class CardLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    card_uid = db.Column(db.String(20), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    user = db.relationship('User', backref=db.backref('card_logs', lazy=True))
 
 with app.app_context():
     db.create_all()
@@ -29,6 +38,11 @@ def get_logged_in_user():
         return User.query.get(user_id)
     return None
 
+def log_card_tap(user_id, card_uid):
+    new_card_log = CardLog(user_id=user_id, card_uid=card_uid)
+    db.session.add(new_card_log)
+    db.session.commit()
+
 @app.route('/')
 def home():
     user = get_logged_in_user()
@@ -41,10 +55,18 @@ def register():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
+        card_uid = request.form.get('card_uid')  # Retrieve card UID from the form
 
         new_user = User(full_name=full_name, username=username, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
+
+        # Log card tap if card UID is provided
+        if card_uid:
+            user = User.query.filter_by(username=username).first()
+            if user:
+                log_card_tap(user.id, card_uid)
+
         return redirect(url_for('login'))
     else:
         return render_template('register.html')
@@ -71,6 +93,15 @@ def profile():
     if is_user_logged_in():
         user = get_logged_in_user()
         return render_template('profile.html', user=user)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/card_log')
+def card_log():
+    user = get_logged_in_user()
+    if user:
+        card_logs = CardLog.query.filter_by(user_id=user.id).all()
+        return render_template('card_log.html', user=user, card_logs=card_logs)
     else:
         return redirect(url_for('login'))
 
