7ad9e92d65a53cdb9b46727c02691b7183e0e0a0
dansyaku7 <syakuronabdan49@gmail.com>
2023-12-26 20:45:26 +0700

    New Update

--- web/crud.py
+++ web/crud.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, Response, redirect, url_for, session
+from flask import Flask, render_template, request, jsonify, Response, redirect, url_for, session, flash
 import time
 import json
 from flask_sqlalchemy import SQLAlchemy
@@ -9,15 +9,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    card_uid = db.Column(db.String(255))  # Kolom baru untuk menyimpan card UID
+    card_uid = db.Column(db.String(255))
+    card_activated = db.Column(db.Boolean, default=False)
 
-# Variabel untuk menyimpan UID
 current_uid = ""
 
 def is_user_logged_in():
@@ -29,13 +36,20 @@ def get_logged_in_user():
         return User.query.get(user_id)
     return None
 
+def is_admin_logged_in():
+    return 'admin_id' in session
+
+def get_logged_in_admin():
+    if is_admin_logged_in():
+        admin_id = session['admin_id']
+        return Admin.query.get(admin_id)
+    return None
+
 @app.route('/')
 def index():
     user = get_logged_in_user()
-    if user:
-        return redirect(url_for('profile'))
-    else:
-        return render_template('home.html')
+    admin = get_logged_in_admin()
+    return render_template('index.html', user=user, admin=admin)
 
 @app.route('/stream')
 def stream():
@@ -62,46 +76,79 @@ def register():
     password = request.form.get('password')
     card_uid = request.form.get('card_uid')
 
-    # Periksa apakah card UID sudah terdaftar
     existing_card_user = User.query.filter_by(card_uid=card_uid).first()
     if existing_card_user:
         return jsonify(success=False, message='Card UID already registered')
 
-    # Periksa apakah username atau email sudah ada di database
     existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
     if existing_user:
         return jsonify(success=False, message='Username or email already exists')
 
-    # Tambahkan pengguna baru ke dalam database
     new_user = User(full_name=full_name, username=username, email=email, password=password, card_uid=card_uid)
     db.session.add(new_user)
     db.session.commit()
 
-    # Redirect ke halaman login setelah registrasi sukses
-    return redirect(url_for('show_login'))
+    return render_template('contact_info.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username_email = request.form.get('username_email')
-        password = request.form.get('password')
+        username_email = request.form['username_email']
+        password = request.form['password']
 
         user = User.query.filter((User.username == username_email) | (User.email == username_email)).first()
+
         if user and user.password == password:
             session['user_id'] = user.id
-            return redirect(url_for('home'))  # Mengarahkan ke halaman home setelah login sukses
+            flash('Login successful', 'success')
+            return redirect(url_for('home'))
+        
+        admin = Admin.query.filter((Admin.username == username_email) | (Admin.email == username_email)).first()
 
-        return render_template('login.html', error='Invalid credentials')
+        if admin and admin.password == password:
+            session['admin_id'] = admin.id
+            flash('Admin login successful', 'success')
+            return redirect(url_for('admin_profile')) 
 
-    return render_template('login.html', error='')
+        flash('Invalid username or password', 'error')
+
+    return render_template('login.html')
 
 @app.route('/home')
 def home():
-    user = get_logged_in_user()
-    if user:
+    user_id = session.get('user_id')
+    admin_id = session.get('admin_id')
+
+    if user_id:
+        user = User.query.get(user_id)
         return render_template('home.html', user=user)
+    elif admin_id:
+        admin = Admin.query.get(admin_id)
+        return render_template('home.html', admin=admin)
     else:
-        return render_template('home.html')
+        return redirect(url_for('login'))
+
+@app.route('/register_admin', methods=['GET', 'POST'])
+def register_admin():
+    if request.method == 'POST':
+        full_name = request.form['full_name']
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        existing_admin = Admin.query.filter((Admin.username == username) | (Admin.email == email)).first()
+        if existing_admin:
+            flash('Username or email already exists', 'error')
+            return redirect(url_for('register_admin'))
+
+        new_admin = Admin(full_name=full_name, username=username, email=email, password=password)
+        db.session.add(new_admin)
+        db.session.commit()
+
+        flash('Admin registration successful', 'success')
+        return redirect(url_for('admin_profile'))
+    else:
+        return render_template('admin_registration.html')
 
 @app.route('/register', methods=['GET'])
 def show_register():
@@ -114,18 +161,50 @@ def show_login():
 @app.route('/profile')
 def profile():
     user = get_logged_in_user()
+    admin = get_logged_in_admin()
+
     if user:
         return render_template('profile.html', user=user)
-    else:
+    elif not admin:
+        flash('Unauthorized access. Please log in as an admin.', 'error')
         return redirect(url_for('login'))
-    
+
+    users = User.query.all()
+
+    return render_template('admin_profile.html', admin=admin, users=users)
+
+@app.route('/admin_profile')
+def admin_profile():
+    admin = get_logged_in_admin()
+
+    if not admin:
+        flash('Unauthorized access. Please log in as an admin.', 'error')
+        return redirect(url_for('login'))
+
+    users = User.query.all()
+
+    return render_template('admin_profile.html', admin=admin, users=users)
+
+@app.route('/toggle_activation/<int:user_id>', methods=['POST'])
+def toggle_activation(user_id):
+    user = User.query.get(user_id)
+
+    if user:
+        user.card_activated = not user.card_activated
+        db.session.commit()
+
+        flash(f'User {user.full_name} is {"Activated" if user.card_activated else "Deactivated"}', 'success')
+    else:
+        flash('User not found', 'error')
+
+    return redirect(url_for('admin_profile'))
+
 @app.route('/logout')
 def logout():
-    # Bersihkan sesi dan redirect ke halaman home
     session.clear()
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
